409288102b6db57434ec3f4f18260466911f34c0
Raphael <raphael.prunonosa@epitech.eu>
2026-07-16 21:24:52 +0200

    feat: dash finished

--- concrete-jungle/scenes/game_scene/player/player_body3d.gd
+++ concrete-jungle/scenes/game_scene/player/player_body3d.gd
@@ -2,7 +2,13 @@ extends CharacterBody3D
 
 @export var move_speed : float = 8.0
 @export var acceleration : float = 20.0
-
+var dash_speed : float = 16.0
+@export var dash_duration : float = 0.2
+@export var dash_cooldown : float = 0.6
+var dash_timer : float = 0.0
+var dash_cooldown_timer : float = 0.0
+var is_dash_active : bool = false
+var dash_dir : Vector3 = Vector3.ZERO
 @export var _gravity := -30.0
 @export var jump_impulse : float = 12.0
 var wall_jump_impulse : float = 12.0
@@ -40,17 +46,18 @@ func _input(event: InputEvent) -> void:
 	
 func get_wall_side():
 	var hit = get_wall_normal()
-	print(hit)
 	last_wall = hit
 	velocity = hit * wall_jump_impulse
 	velocity.y = wall_jump_impulse
 
 
+var dash_pressed_last_frame : bool = false
+
 func _physics_process(delta: float) -> void:
 	# twist pivot up/down
 	_camera_twist.rotation.x -= _camera_input_direction.y * delta
 	_camera_twist.rotation.x = clamp(_camera_pivot.rotation.x, -PI / 3.0, PI / 3.0)
-#player left/right
+	#player left/right
 	self.rotation.y -= _camera_input_direction.x * delta
 	
 	_camera_input_direction = Vector2.ZERO
@@ -65,33 +72,57 @@ func _physics_process(delta: float) -> void:
 	right = right.normalized()
 	
 	var move_direction := forward * move_input.y + right * move_input.x
-	
-	
+	print(move_direction)
 	var y_velocity := velocity.y
 	velocity.y = 0.0
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
-	 
 	
 	if not is_on_floor():
 		velocity.y = y_velocity + _gravity * delta
 		
 	if last_wall == null or (get_wall_normal() != last_wall):
 		jumped = 0
+		
+
 	var is_jumping := InputActions.is_jump_pressed() and is_on_floor()
 	var is_on_ground := is_on_floor()
+	var is_dashing := InputActions.is_dash_pressed()
 	var is_wall_jumping := InputActions.is_jump_pressed() and !is_on_floor() and is_on_wall() and jumped == 0
-
+	
+	
 	if is_jumping:
 		velocity.y = jump_impulse
 	if is_on_ground:
 		jumped = 0
 		last_wall = null
+	if is_on_wall() and !is_on_floor():
+		velocity.y = -5
+		last_wall = get_wall_normal()
 	if is_wall_jumping:
+		last_wall = get_wall_normal()
 		jumped = 1
 		get_wall_side()
 		#add sound
+
+
+
+	if dash_cooldown_timer > 0.0:
+		dash_cooldown_timer -= delta
+	if is_dashing and dash_cooldown_timer <= 0.0 and not is_dash_active:
+		is_dash_active = true
+		dash_timer = dash_duration
+		dash_cooldown_timer = dash_cooldown
+		dash_dir = self.global_transform.basis.x
+	if is_dash_active:
+		velocity.x = dash_dir.x * dash_speed
+		velocity.z = dash_dir.z * dash_speed
+		velocity.y = 0.0
+		dash_timer -= delta
+		if dash_timer <= 0.0:
+			is_dash_active = false
 	
 	move_and_slide()
-
+	
+	#var ground_speed := velocity.lenght()
 	
 	#var ground_speed := velocity.lenght()
